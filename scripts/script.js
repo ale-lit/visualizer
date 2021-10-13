2a731de628a4dbec59e01c62ff295fa9588d132f
ZeldoR <zeldor@ya.ru>
2021-10-14 00:34:54 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,13 @@
 let container = document.querySelector('.root__container');
-let all = '1000';
+let all = '2000';
 let progress = '35';
 
-for (let i = 0; i < all; i++) {
-    if (i < progress) {
-        container.innerHTML += '<span class="square square_active"></span>';
+for (let i = 1; i <= all; i++) {
+    if (i <= progress) {
+        container.insertAdjacentHTML('beforeend', '<span title="' + i + '" class="square square_active"></span>');
+        //container.innerHTML += '<span class="square square_active"></span>';
     } else {
-        container.innerHTML += '<span class="square"></span>';
+        container.insertAdjacentHTML('beforeend', '<span title="' + i + '" class="square"></span>');
+        //container.innerHTML += '<span class="square"></span>';
     }
 }
