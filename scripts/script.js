0e3227e10296d6c56351e8c7689f5af0ab08ef79
ZeldoR <zeldor@ya.ru>
2021-10-20 15:39:00 +0300

    feat: add date functional

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,12 @@
-let container = document.querySelector('.root__container');
-let all = '2000';
-let progress = '35';
+// Get current time
+let nowDate = new Date();
 
-for (let i = 1; i <= all; i++) {
-    if (i <= progress) {
-        container.insertAdjacentHTML('beforeend', '<span title="' + i + '" class="square square_active"></span>');
-        //container.innerHTML += '<span class="square square_active"></span>';
-    } else {
-        container.insertAdjacentHTML('beforeend', '<span title="' + i + '" class="square"></span>');
-        //container.innerHTML += '<span class="square"></span>';
-    }
-}
+let startDate;
+let endDate;
 
-
-let startTime = document.querySelector('#from').value;
-
-//startTime.value = '2017-06-01T08:30';
-
-let test = document.querySelector('#test');
-
-function testAlert() {
-    alert(($('#to').datepicker("getDate") - $('#from').datepicker("getDate")) / (24 * 3600 * 1000));
-}
-
-test.addEventListener('click', testAlert);
-
-
-var date = new Date();
-console.log(date);
-
-
+// Calendar widget
 $( function() {
-
-
+    // Options
     $.datepicker.setDefaults({
         dayNamesMin: [ "Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб" ],
         monthNames: [ "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" ],
@@ -41,40 +15,66 @@ $( function() {
         changeYear: true
     });
 
-    //var monthNames = $("#from").datepicker( "option", "monthNames" );
-
     var dateFormat = "dd.mm.yy",
-      from = $( "#from" )
+        from = $( "#from" )
         .datepicker({
-          defaultDate: "+1w",
-          changeMonth: true
+            defaultDate: "+1w",
+            changeMonth: true
         })
         .on( "change", function() {
-          to.datepicker( "option", "minDate", getDate( this ) );
+            to.datepicker("option", "minDate", getDate(this));
+            startDate = $(this).datepicker("getDate");
+            createVisualize();
         }),
-      to = $( "#to" ).datepicker({
-        defaultDate: "+1w",
-        changeMonth: true
-      })
-      .on( "change", function() {
-        from.datepicker( "option", "maxDate", getDate( this ) );
-      });
- 
-    function getDate( element ) {
-      var date;
-      try {
-        date = $.datepicker.parseDate( dateFormat, element.value );
-      } catch( error ) {
-        date = null;
-      }
- 
-      return date;
-    }
-  } );
+        to = $( "#to" ).datepicker({
+            defaultDate: "+1w",
+            changeMonth: true
+        })
+        .on( "change", function() {
+            from.datepicker("option", "maxDate", getDate(this));
+            endDate = $(this).datepicker("getDate");
+            createVisualize();
+        });
 
-//   $.datepicker.formatDate( "DD, MM d, yy", new Date( 2007, 7 - 1, 14 ), {
-//     dayNamesShort: $.datepicker.regional[ "fr" ].dayNamesShort,
-//     dayNames: $.datepicker.regional[ "fr" ].dayNames,
-//     monthNamesShort: $.datepicker.regional[ "fr" ].monthNamesShort,
-//     monthNames: $.datepicker.regional[ "fr" ].monthNames
-//   });
+    function getDate( element ) {
+        var date;
+        try {
+            date = $.datepicker.parseDate( dateFormat, element.value );
+        } catch( error ) {
+            date = null;
+        }
+        
+        return date;
+    }
+});
+
+
+// Construct visual
+const container = document.querySelector('.root__container');
+
+function createVisualize() {
+    let all = (endDate - startDate) / (24 * 3600 * 1000);
+    let progress = (nowDate - startDate) / (24 * 3600 * 1000);
+    //alert(progress);
+
+    for (let i = 1; i <= all; i++) {
+        if (i <= progress) {
+            container.insertAdjacentHTML('beforeend', '<span title="' + i + '" class="square square_active"></span>');
+            //container.innerHTML += '<span class="square square_active"></span>';
+        } else {
+            container.insertAdjacentHTML('beforeend', '<span title="' + i + '" class="square"></span>');
+            //container.innerHTML += '<span class="square"></span>';
+        }
+    }
+}
+
+
+
+
+// FOR DEBAGGING ***********
+let test = document.querySelector('#test');
+function testAlert() {
+    alert((nowDate - $('#from').datepicker("getDate"))  / (24 * 3600 * 1000));
+    //alert(($('#to').datepicker("getDate") - $('#from').datepicker("getDate")) / (24 * 3600 * 1000));
+}
+test.addEventListener('click', testAlert);
