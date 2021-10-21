ede23f344c4866cec28b581bfb911b0080440c81
ZeldoR <zeldor@ya.ru>
2021-10-21 08:49:59 +0300

    refact: formatting code

--- scripts/script.js
+++ scripts/script.js
@@ -5,101 +5,110 @@ let startDate;
 let endDate;
 
 // Calendar widget
-$( function() {
-    // Options
-    $.datepicker.setDefaults({
-        dayNamesMin: [ 'Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб' ],
-        monthNames: [ 'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь' ],
-        firstDay: 1,
-        dateFormat: "dd.mm.yy",
-        changeYear: true
-    });
+$(function () {
+  // Options
+  $.datepicker.setDefaults({
+    dayNamesMin: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+    monthNames: [
+      "Январь",
+      "Февраль",
+      "Март",
+      "Апрель",
+      "Май",
+      "Июнь",
+      "Июль",
+      "Август",
+      "Сентябрь",
+      "Октябрь",
+      "Ноябрь",
+      "Декабрь",
+    ],
+    firstDay: 1,
+    dateFormat: "dd.mm.yy",
+    changeYear: true,
+  });
 
-    var dateFormat = 'dd.mm.yy',
-        from = $('#from')
-        .datepicker({
-            defaultDate: '+1w',
-            changeMonth: true
-        })
-        .on('change', function() {
-            to.datepicker('option', 'minDate', getDate(this));
-            startDate = $(this).datepicker('getDate');
-            if(endDate) createVisualize(startDate, endDate);
-        }),
-        to = $('#to').datepicker({
-            defaultDate: '+1w',
-            changeMonth: true
-        })
-        .on("change", function() {
-            from.datepicker('option', 'maxDate', getDate(this));
-            endDate = $(this).datepicker('getDate');
-            if(startDate) createVisualize(startDate, endDate);
-        });
+  var dateFormat = "dd.mm.yy",
+    from = $("#from")
+      .datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+      })
+      .on("change", function () {
+        to.datepicker("option", "minDate", getDate(this));
+        startDate = $(this).datepicker("getDate");
+        if (endDate) createVisualize(startDate, endDate);
+      }),
+    to = $("#to")
+      .datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+      })
+      .on("change", function () {
+        from.datepicker("option", "maxDate", getDate(this));
+        endDate = $(this).datepicker("getDate");
+        if (startDate) createVisualize(startDate, endDate);
+      });
 
-    function getDate( element ) {
-        var date;
-        try {
-            date = $.datepicker.parseDate( dateFormat, element.value );
-        } catch( error ) {
-            date = null;
+  function getDate(element) {
+    var date;
+    try {
+      date = $.datepicker.parseDate(dateFormat, element.value);
+    } catch (error) {
+      date = null;
+    }
+
+    return date;
+  }
+
+  // Construct visual
+  function createVisualize(start, end) {
+    if (start && end) {
+      // Clear container
+      $(".visual-block").html("");
+
+      // Calculate number of items and progress
+      //let all = (end - start) / (24 * 3600 * 1000);
+      //let progress = (now - start) / (24 * 3600 * 1000);
+
+      // test
+      let all = 51;
+      let progress = 2;
+
+      // Calculate size work area
+      let vBlockWidth = $(document).width() - 50 - 3;
+      if (vBlockWidth > 1227) vBlockWidth = 1227;
+      let vBlockHeight = $(document).height() - 190 - 3;
+
+      // Calculate item size
+      let itemSize = (vBlockWidth * vBlockHeight) / all;
+      itemSize = Math.floor(Math.sqrt(itemSize) - 3);
+      if (itemSize < 1) {
+        itemSize = 1;
+      } else if (itemSize > 50) {
+        itemSize = 50;
+      }
+
+      // Add items
+      for (let i = 1; i <= all; i++) {
+        if (i <= progress) {
+          $(".visual-block").append(
+            `<span title="${i}" class="square square_active" style="width: ${itemSize}px; height: ${itemSize}px;"></span>`
+          );
+        } else {
+          $(".visual-block").append(
+            `<span title="${i}" class="square" style="width: ${itemSize}px; height: ${itemSize}px;"></span>`
+          );
         }
-        
-        return date;
+      }
+
+      // Display result
+      $(".setting").fadeOut();
+      $(".visual-block").css("display", "flex");
     }
+  }
 
-
-    // Construct visual
-    function createVisualize(start, end) {
-        if(start && end) {
-            // Clear container
-            $('.visual-block').html('');
-
-            // Calculate number of items and progress
-            //let all = (end - start) / (24 * 3600 * 1000);
-            //let progress = (now - start) / (24 * 3600 * 1000);
-
-            // test
-            let all = 51;
-            let progress = 2;
-            
-            // Calculate size work area
-            let vBlockWidth = $(document).width() - 50 - 3;
-            if(vBlockWidth > 1227) vBlockWidth = 1227;
-            let vBlockHeight = $(document).height() - 190 - 3;
-
-            // Calculate item size
-            let itemSize = (vBlockWidth * vBlockHeight) / all;
-            itemSize = Math.floor(Math.sqrt(itemSize) - 3);
-            if (itemSize < 1) {
-                itemSize = 1;
-            } else if (itemSize > 50) {
-                itemSize = 50;
-            };
-
-            // Add items
-            for (let i = 1; i <= all; i++) {
-                if (i <= progress) {
-                    $('.visual-block').append(`<span title="${i}" class="square square_active" style="width: ${itemSize}px; height: ${itemSize}px;"></span>`);
-                } else {
-                    $('.visual-block').append(`<span title="${i}" class="square" style="width: ${itemSize}px; height: ${itemSize}px;"></span>`);
-                }
-            }
-
-            // Display result
-            $('.setting').fadeOut();            
-            $('.visual-block').css('display', 'flex');
-        }
-    }
-
-
-    
-
-
-
-    // FOR DEBAG ***********
-    function testAlert() {
-
-    }
-    $('#test').on('click', testAlert);
-
+  // FOR DEBAG ***********
+  function testAlert() {}
+  $("#test").on("click", testAlert);
 });
